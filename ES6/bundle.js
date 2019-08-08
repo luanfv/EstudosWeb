35c6fe50fbb79fd112336f18c485a4c23647e5f5
Luan F.Vieira <luanfrancavieira@gmail.com>
2019-08-08 09:15:15 -0300

    REST e SPREAD com JavaScript ES6

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,5 +1,15 @@
 "use strict";
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
@@ -84,4 +94,46 @@ var usuario = {
 var nome = usuario.nome,
     idade = usuario.idade,
     cidade = usuario.endereco.cidade;
-console.log(nome, idade, cidade);
+console.log(nome, idade, cidade); // REST
+
+var pessoa = {
+  sexo: 'M',
+  idade: 19,
+  alutra: 1.65
+};
+
+var sexo = pessoa.sexo,
+    resto = _objectWithoutProperties(pessoa, ["sexo"]);
+
+console.log(sexo, resto);
+var array1 = [1, 2, 3, 4];
+var array2 = [5, 6, 7, 8];
+var a = array1[0],
+    b = array1[1],
+    c = array1.slice(2);
+console.log(a, b, c);
+
+function soma(a) {
+  for (var _len = arguments.length, params = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    params[_key - 1] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  }) - a;
+}
+
+console.log(soma(1, 2, 3)); // SPREAD
+
+var array3 = [].concat(array1, array2);
+console.log(array3);
+var usuario1 = {
+  nome: "Gerald",
+  idade: 37
+};
+
+var usuario2 = _objectSpread({}, usuario1, {
+  idade: 49
+});
+
+console.log(usuario1, usuario2);
